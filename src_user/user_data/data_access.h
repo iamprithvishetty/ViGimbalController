14c75fe996452e59f1d24d2eae52d78d7eb7ad8b
iamprithvishetty <prithvi@cdspace.in>
2022-09-20 01:04:40 +0530

    add user_conf_data and then extern in .h

--- src_user/user_data/data_access.h
+++ src_user/user_data/data_access.h
@@ -27,46 +27,7 @@ typedef struct{
 
 }data_access;
 
-/*
- * This is to store the valid incoming message from the user
- */
-data_access user_conf_data[] = {
-
-    {.message = "PWR_PITCH"    , .type = INTEGER,   .data_int   = &motor_pitch.power,      .data_int_min = 0,     .data_int_max = 100},
-    {.message = "PP_PITCH"     , .type = INTEGER,   .data_int   = &motor_pitch.pole_pair,  .data_int_min = 0,     .data_int_max = 20},   
-    {.message = "DIR_PITCH"    , .type = INTEGER,   .data_int   = &motor_pitch.direction,  .data_int_min = -1,    .data_int_max = 1},
-    {.message = "P_ANG_PITCH"  , .type = FLOATING,  .data_float = &pid_pitch_angle.kp,     .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "P_ROT_PITCH"  , .type = FLOATING,  .data_float = &pid_pitch_rotation.kp,  .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "I_ROT_PITCH"  , .type = FLOATING,  .data_float = &pid_pitch_rotation.ki,  .data_float_min = 0,   .data_float_max = 1000},   
-    {.message = "D_ROT_PITCH"  , .type = FLOATING,  .data_float = &pid_pitch_rotation.kd,  .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "ANGLE_PITCH"  , .type = FLOATING,  .data_float = &user_pitch_angle,       .data_float_min = -90, .data_float_max = 90},
-
-    {.message = "PWR_ROLL"    , .type = INTEGER,    .data_int   = &motor_roll.power,       .data_int_min = 0,     .data_int_max = 100},
-    {.message = "PP_ROLL"     , .type = INTEGER,    .data_int   = &motor_roll.pole_pair,   .data_int_min = 0,     .data_int_max = 20},   
-    {.message = "DIR_ROLL"    , .type = INTEGER,    .data_int   = &motor_roll.direction,   .data_int_min = -1,    .data_int_max = 1},
-    {.message = "P_ANG_ROLL"  , .type = FLOATING,   .data_float = &pid_roll_angle.kp,      .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "P_ROT_ROLL"  , .type = FLOATING,   .data_float = &pid_roll_rotation.kp,   .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "I_ROT_ROLL"  , .type = FLOATING,   .data_float = &pid_roll_rotation.ki,   .data_float_min = 0,   .data_float_max = 1000},   
-    {.message = "D_ROT_ROLL"  , .type = FLOATING,   .data_float = &pid_roll_rotation.kd,   .data_float_min = -1,  .data_float_max = 1000},
-
-    {.message = "PWR_YAW"     , .type = INTEGER,    .data_int   = &motor_yaw.power,        .data_int_min = 0,     .data_int_max = 100},
-    {.message = "PP_YAW"      , .type = INTEGER,    .data_int   = &motor_yaw.pole_pair,    .data_int_min = 0,     .data_int_max = 20},   
-    {.message = "DIR_YAW"     , .type = INTEGER,    .data_int   = &motor_yaw.direction,    .data_int_min = -1,    .data_int_max = 1},
-    {.message = "P_ROT_YAW"   , .type = FLOATING,   .data_float = &pid_yaw_rotation.kp,    .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "I_ROT_YAW"   , .type = FLOATING,   .data_float = &pid_yaw_rotation.ki,    .data_float_min = 0,   .data_float_max = 1000},   
-    {.message = "D_ROT_YAW"   , .type = FLOATING,   .data_float = &pid_yaw_rotation.kd,    .data_float_min = -1,  .data_float_max = 1000},
-
-    {.message = "DEBUG"        , .type = INTEGER,   .data_int   = &debug,                  .data_int_min = 0,     .data_int_max = 1},
-
-    {.message = "RESISTOR_UP"  , .type = FLOATING,  .data_float = &resistor_up,          .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "RESISTOR_DOWN", .type = FLOATING,  .data_float = &resistor_down,        .data_float_min = 0,   .data_float_max = 1000},
-    {.message = "BATT_VOLT"    , .type = FLOATING,  .data_float = &battery_voltage,      .data_float_min = 0,   .data_float_max = 0},
-    {.message = "BATT_APLHA"   , .type = FLOATING,  .data_float = &battery_voltage_alpha,.data_float_min = 0,   .data_float_max = 1},    
-
-    {.message = "ENABLE_IMU2"      , .type = INTEGER,   .data_int   = &imu_platform_enable,.data_int_min = 0,     .data_int_max = 1},
-    {.message = "IMU1_ORIENTATION" , .type = INTEGER,   .data_int   = &imu_cam.orientation,.data_int_min = 1,     .data_int_max = 24},
-    {.message = "IMU2_ORIENTATION" , .type = INTEGER,   .data_int   = &imu_cam.orientation,.data_int_min = 1,     .data_int_max = 24},
-
-};
+extern data_access user_conf_data[];
+extern const int length_user_conf_data;
 
 #endif
